60f87d264bf320382a1dffe4509597d7ae99fa51
relative-sumit <sumitsix42@gmail.com>
2024-05-21 19:45:21 +0530

    added changes

--- dbHandler/updateEmployeeInfo.js
+++ dbHandler/updateEmployeeInfo.js
@@ -1,14 +1,13 @@
 const EmployeeInfo = require('../models/employeeInfo');
 
 async function updateEmployeeInfo(UserId, Username, input){
-    // console.log(input)
+    // console.log(UserId)
     try {
         const date  = new Date();
-        const getEmployeeById = await EmployeeInfo.find({_id: UserId, IsActive: 1, IsDeleted: 0});
         input.UpdatedBy = Username,
         input.UpdatedDate = date.toLocaleDateString()+" "+date.toLocaleTimeString();
         // input.Email.CompanyMail = await getEmployeeById[0].Email.CompanyMail
-        const updateEmployee = await EmployeeInfo.findOneAndUpdate({_id: UserId, IsActive: 1, IsDeleted:0}, input, {new: true, runValidators: true});
+        const updateEmployee = await EmployeeInfo.findOneAndUpdate({UserId: UserId, IsActive: 1, IsDeleted:0}, input, {new: true, runValidators: true});
         if(!updateEmployee){
             throw new Error(`Employee not found with ${UserId}`);
         }
